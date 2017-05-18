8adbd0d0ecc5cc60ba5e7c12d1b5e269e43bd797
Daniel Wilkes <daniel_wilkes@outlook.com>
2017-05-18 18:13:05 +0100

    test

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ class HeaderBar extends React.Component{
         <button className="homeButton"></button>
 
         <div className="headerButtons">
-          <a className="headerButton">a</button>
-          <a className="headerButton">b</button>
-          <a className="headerButton">c</button>
-          <a className="headerButton">d</button>
-          <a className="headerButton">e</button>
-          <a className="headerButton">f</button>
-          <a className="headerButton">g</button>
+          <a className="headerButton">a</a>
+          <a className="headerButton">a</a>
+          <a className="headerButton">a</a>
+          <a className="headerButton">a</a>
+          <a className="headerButton">a</a>
+          <a className="headerButton">a</a>
+          <a className="headerButton">a</a>
         </div>
       </div>
     );
